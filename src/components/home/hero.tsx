9b905254b4c3cb02966923c4f896bd987ac63ee4
Md Aafaque Tabish <gg.aafaque@gmail.com>
2026-07-07 21:50:55 +0530

    Remove hero bottom gradient fade

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -158,12 +158,6 @@ export function Hero() {
           </motion.div>
         </div>
       </div>
-
-      {/* Bottom gradient fade */}
-      <div
-        aria-hidden="true"
-        className="pointer-events-none absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent"
-      />
     </section>
   );
 }
